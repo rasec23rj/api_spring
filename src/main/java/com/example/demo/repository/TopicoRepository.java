6e604a771cdbabd5cf940c3b910aabc75815cd4e
jcbarcelos <jcbarcelosrj@gmail.com>
2021-05-26 12:49:51 -0300

    refactor: param("nome") string nome

--- src/main/java/com/example/demo/repository/TopicoRepository.java
+++ src/main/java/com/example/demo/repository/TopicoRepository.java
@@ -1,11 +1,8 @@
 package com.example.demo.repository;
 
 import java.util.List;
-
 import com.example.demo.model.Topico;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
@@ -17,7 +14,7 @@ public interface TopicoRepository extends JpaRepository<Topico, Long> {
      * Busca personalizada !
      * 
      * @Query("select t from Topico t where t.curso.nome = :nome") List<Topico>
-     * carregarPorNomeDoCurso(String nome);
+     * carregarPorNomeDoCurso(@Param("nome") String nome);
      */
 
 }
