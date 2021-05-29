91fbbd66d59a9ba969c23b700bf4eb83957c9f10
jcbarcelos <jcbarcelosrj@gmail.com>
2021-05-29 09:03:14 -0300

    feat: add delete topico

--- src/main/java/com/example/demo/controller/TopicosController.java
+++ src/main/java/com/example/demo/controller/TopicosController.java
@@ -15,6 +15,7 @@ import com.example.demo.repository.CursoRepository;
 import com.example.demo.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -81,4 +82,11 @@ public class TopicosController {
         return ResponseEntity.ok(new TopicoDto(topico));
     }
 
+    @DeleteMapping(path = "/{id}")
+    @Transactional
+    public ResponseEntity<?> remove(@PathVariable Long id) {
+        topicoRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+        
+    }
 }
